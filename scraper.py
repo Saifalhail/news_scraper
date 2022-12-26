0a8ee7ae262ace3e03464427e5dc1c960981838b
Saif Alhail <sa22431@essex.ac.uk>
2022-12-26 15:42:13 +0300

    DONE

--- scraper.py
+++ scraper.py
@@ -2,46 +2,50 @@ import newspaper
 from newspaper import Article
 from datetime import datetime
 
-list_of_urls_mena = ['https://www.aljazeera.com/news/2022/12/6/n-korea-orders-new-artillery-firings-over-us-s-korea-drills',
-				'https://www.aljazeera.com/news/2022/12/5/un-delays-decision-on-myanmar-representation-over-russian-stance',
-				'https://www.aljazeera.com/news/2022/12/3/u-s-unveils-new-700m-high-tech-b-21-stealth-bomber',
-                'https://www.bbc.com/news/world-europe-63857451']
+# list_of_urls_mena = ['https://www.aljazeera.com/news/2022/12/6/n-korea-orders-new-artillery-firings-over-us-s-korea-drills',
+# 				'https://www.aljazeera.com/news/2022/12/5/un-delays-decision-on-myanmar-representation-over-russian-stance',
+# 				'https://www.aljazeera.com/news/2022/12/3/u-s-unveils-new-700m-high-tech-b-21-stealth-bomber',
+#                 'https://www.bbc.com/news/world-europe-63857451']
 
-list_of_urls_int = ['https://www.aljazeera.com/economy/2022/12/13/chinese-property-tycoon-accused-of-bribing-officials-in-us',
-                'https://www.aljazeera.com/economy/2022/12/13/ftxs-bankman-fried-arrested-in-the-bahamas-as-us-unveils-charges',
-                'https://www.aljazeera.com/economy/2022/12/12/cvs-walgreens-finalise-10bn-in-deals-to-settle-opioid-lawsuits']
+# list_of_urls_int = ['https://www.aljazeera.com/economy/2022/12/13/chinese-property-tycoon-accused-of-bribing-officials-in-us',
+#                 'https://www.aljazeera.com/economy/2022/12/13/ftxs-bankman-fried-arrested-in-the-bahamas-as-us-unveils-charges',
+#                 'https://www.aljazeera.com/economy/2022/12/12/cvs-walgreens-finalise-10bn-in-deals-to-settle-opioid-lawsuits']
 
-list_of_urls_proc = ['https://www.aljazeera.com/sports/2022/12/12/can-morocco-win-the-world-cup',
-                'https://www.aljazeera.com/news/2022/12/11/the-ten-talking-points-of-the-world-cup-so-far',
-                'https://www.aljazeera.com/sports/2022/12/12/the-freestyle-footballer-with-a-difference-wowing-world-cup-fans']
+# list_of_urls_proc = ['https://www.aljazeera.com/sports/2022/12/12/can-morocco-win-the-world-cup',
+#                 'https://www.aljazeera.com/news/2022/12/11/the-ten-talking-points-of-the-world-cup-so-far',
+#                 'https://www.aljazeera.com/sports/2022/12/12/the-freestyle-footballer-with-a-difference-wowing-world-cup-fans']
 
-extracted1 = {'articles': []}
-extracted2 = {'articles': []}
-extracted3 = {'articles': []}
+def scraper(list_of_urls_mena, list_of_urls_int, list_of_urls_proc):
 
-for url in list_of_urls_mena:
-    article = newspaper.Article(url="%s" % (url), language='en')
-    article.download()
-    article.parse()
-    if article.publish_date is not None:
-        publish_date = datetime.strptime(str(article.publish_date), '%Y-%m-%d %H:%M:%S').strftime('%d-%b-%Y')
-    obj = {'title': article.title, 'top_image': article.top_image, 'publish_date': publish_date, 'url': article.url}
-    extracted1['articles'].append(obj)
+    extracted1 = {'articles': []}
+    extracted2 = {'articles': []}
+    extracted3 = {'articles': []}
 
-for url in list_of_urls_int:
-    article = newspaper.Article(url="%s" % (url), language='en')
-    article.download()
-    article.parse()
-    if article.publish_date is not None:
-        publish_date = datetime.strptime(str(article.publish_date), '%Y-%m-%d %H:%M:%S').strftime('%d-%b-%Y')
-    obj = {'title': article.title, 'top_image': article.top_image, 'publish_date': publish_date, 'url': article.url}
-    extracted2['articles'].append(obj)
+    for url in list_of_urls_mena:
+        article = newspaper.Article(url="%s" % (url), language='en')
+        article.download()
+        article.parse()
+        if article.publish_date is not None:
+            publish_date = datetime.strptime(str(article.publish_date), '%Y-%m-%d %H:%M:%S').strftime('%d-%b-%Y')
+        obj = {'title': article.title, 'top_image': article.top_image, 'publish_date': publish_date, 'url': article.url}
+        extracted1['articles'].append(obj)
 
-for url in list_of_urls_proc:
-    article = newspaper.Article(url="%s" % (url), language='en')
-    article.download()
-    article.parse()
-    if article.publish_date is not None:
-        publish_date = datetime.strptime(str(article.publish_date), '%Y-%m-%d %H:%M:%S').strftime('%d-%b-%Y')
-    obj = {'title': article.title, 'top_image': article.top_image, 'publish_date': publish_date, 'url': article.url}
-    extracted3['articles'].append(obj)
+    for url in list_of_urls_int:
+        article = newspaper.Article(url="%s" % (url), language='en')
+        article.download()
+        article.parse()
+        if article.publish_date is not None:
+            publish_date = datetime.strptime(str(article.publish_date), '%Y-%m-%d %H:%M:%S').strftime('%d-%b-%Y')
+        obj = {'title': article.title, 'top_image': article.top_image, 'publish_date': publish_date, 'url': article.url}
+        extracted2['articles'].append(obj)
+
+    for url in list_of_urls_proc:
+        article = newspaper.Article(url="%s" % (url), language='en')
+        article.download()
+        article.parse()
+        if article.publish_date is not None:
+            publish_date = datetime.strptime(str(article.publish_date), '%Y-%m-%d %H:%M:%S').strftime('%d-%b-%Y')
+        obj = {'title': article.title, 'top_image': article.top_image, 'publish_date': publish_date, 'url': article.url}
+        extracted3['articles'].append(obj)
+
+    return extracted1, extracted2, extracted3
--- views.py
+++ views.py
@@ -1,12 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from datetime import datetime
-from scraper import extracted1, extracted2, extracted3
+from scraper import scraper
 
 
 pull_date = datetime.now().strftime('%d-%b-%Y')
 
 views = Blueprint(__name__, "views")
 
+
+
+
+
 @views.route("/")
 def home():
+    return render_template("homepage.html")
+
+
+@views.route("/result", methods=['POST'])
+def result():
+    list_of_urls_mena = []
+    list_of_urls_int = []
+    list_of_urls_proc = []
+    for i in request.form.getlist('mena-url'):
+        list_of_urls_mena.append(i)
+    for i in request.form.getlist('int-url'):
+        list_of_urls_int.append(i)
+    for i in request.form.getlist('proc-url'):
+        list_of_urls_proc.append(i)    
+    extracted1, extracted2, extracted3 = scraper(list_of_urls_mena, list_of_urls_int, list_of_urls_proc)
     return render_template("index.html", date=pull_date, articles_mena=extracted1, articles_int=extracted2, articles_proc=extracted3)
+
+
